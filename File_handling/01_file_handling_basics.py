26af49da98807c87f8a812a6c579251771b15143
anjishkr7 <anjish7004254194@gmail.com>
2026-06-20 19:27:42 +0530

    added basics of file handling

---
+++ File_handling/01_file_handling_basics.py
@@ -0,0 +1,18 @@
+#File handling
+
+# Read Files:-
+
+# p=open(r"C:\Users\Anjish kumar\OneDrive\Desktop\newfile.txt")
+# print(p.read())
+# p=open('File_handling/01_file_handling_basics.py')
+# print(p.read())
+
+# Create Files(w) :-
+# w=open("superman.txt",'w')
+# w.write("Hello this is Anjish!")
+# w.close()
+
+#append(a):-
+# a=open('superman.txt','a')
+# a.write('And i am appending some content inside this file')
+# a.close()

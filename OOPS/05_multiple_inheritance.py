87fac5acf03d49fd7883adf6a60917cdee1bf7fe
anjishkr7 <anjish7004254194@gmail.com>
2026-06-22 12:47:00 +0530

    updated multiple inheritance using instance variable

--- OOPS/05_multiple_inheritance.py
+++ OOPS/05_multiple_inheritance.py
@@ -1,8 +1,16 @@
 class Animal:
-  name1="lion"
+  def __init__(self,name):
+    self.name=name
 class Human:
-  name2="Anjish"
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
 class Robots(Animal,Human):
-  name3="Charlie123"
-obj=Robots()
-print(obj.name1)
+  def __init__(self, name,age,gender):
+    # super().__init__(name)
+    Human.__init__(self,name,age)
+    self.gender=gender
+
+
+obj=Robots("Lion",32,"Female")
+print(obj.name)
